65e0777dc8393a197d4333a582193c69df2d45f5
Lucas <141663051+lucas-devop@users.noreply.github.com>
2025-04-21 09:42:45 +0200

    Update secure_claim_server.js

--- secure_claim_server.js
+++ secure_claim_server.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { ethers } = require("ethers");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1363779241942188052/tfs6lEe_RLMuWM4gfMQmN1uPbAvnJRWawr3TFIT1QPQy4oa_8eqS1VDyZEzcg4ps-csC";
 
 app.use(cors({
   origin: [
@@ -22,7 +25,6 @@ if (!operatorPrivateKey) throw new Error("❌ not found OPERATOR_PRIVATE_KEY in 
 const wallet = new ethers.Wallet(operatorPrivateKey);
 console.log("🔐 Backend is signing as address:", wallet.address);
 
-
 app.post("/sign", async (req, res) => {
   const { address, amount } = req.body;
 
@@ -39,6 +41,22 @@ app.post("/sign", async (req, res) => {
 
     const signature = await wallet.signMessage(ethers.getBytes(rawMessageHash));
 
+    await axios.post(DISCORD_WEBHOOK_URL, {
+      embeds: [
+        {
+          title: `\u2728 Someone just claimed`,
+          description: `Claim has been signed and tokens sent to the wallet!`,
+          color: 0x1abc9c,
+          fields: [
+            { name: "Address", value: address, inline: true },
+            { name: "Amount", value: `${amount} FOALCA`, inline: true },
+            { name: "Nonce", value: nonce.toString(), inline: false }
+          ],
+          timestamp: new Date().toISOString()
+        }
+      ]
+    });
+
     return res.json({ signature, nonce });
   } catch (err) {
     console.error("Signing error:", err);
@@ -47,5 +65,5 @@ app.post("/sign", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`✅ Claim-signing server listening at http://localhost:${port}`);
+  console.log(`\u2705 Claim-signing server listening at http://localhost:${port}`);
 });
